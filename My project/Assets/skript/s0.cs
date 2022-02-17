b20731f3d532148bdc9d16cc62cf38d3497aa5c0
Nikit335 <99726413+Nikit335@users.noreply.github.com>
2022-02-17 14:54:21 +0500

    1
    This reverts commit a258b3858236e48219396b2eff1ac844c6ce4b0d.

---
+++ My project/Assets/skript/s0.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class s0 : MonoBehaviour
+{
+    public Text obj;
+    // Start is called before the first frame update
+    void Start() {
+
+
+    }
+
+   
+    // Update is called once per frame
+    void Update() { 
+    
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            obj.text = "1%";
+        } 
+        
+
+    }
+}
---
+++ My project/Assets/skript/s5.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class s5 : MonoBehaviour
+{
+    public Text obj;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            obj.text = "25%";
+        }
+    }
+}
